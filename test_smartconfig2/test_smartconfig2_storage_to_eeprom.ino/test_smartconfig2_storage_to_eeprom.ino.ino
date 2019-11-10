cf4b77833735874127f6a74326d4d9fea5cd6b08
huyluu <huy.luu.it@gmail.com>
2019-11-10 10:49:55 +0700

    port webserver to module
    still have some major bug

--- test_smartconfig2/test_smartconfig2_storage_to_eeprom.ino/test_smartconfig2_storage_to_eeprom.ino.ino
+++ test_smartconfig2/test_smartconfig2_storage_to_eeprom.ino/test_smartconfig2_storage_to_eeprom.ino.ino
@@ -1,14 +1,17 @@
 #include <IOXhop_FirebaseESP32.h>
 #include <EEPROM.h>
+#include <SPI.h>
 #include "WiFi.h"
+#include "WebServerModule.h"
 #include "SmartConfigModule.h"
 
 #define FIREBASE_HOST "hubbie-viper.firebaseio.com"
-#define FIREBASE_AUTH ""
+#define FIREBASE_AUTH "
 
 int byte1, byte2, byte3, byte4;
 
 void setup() {
+  
   // put your setup code here, to run once:
   Serial.begin(115200);
   EEPROM.begin(512);
@@ -22,26 +25,33 @@ void setup() {
     delay(300);
     Serial.println("Success to connect!");
     // Xuất địa chỉ IP được cấp bởi thiết bị router
+
+    IPAddress ip(WiFi.localIP());
+    IPAddress gateway(192,168,1,1);
+    IPAddress subnet(255,255,255,0);
+    IPAddress ipServer(192,168,137,153);
+    clientConnectToServer(80, ipServer);
+//    Server setup
+//    setStaticIP(ip, gateway, subnet);
+//    initialServer(80);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
-    byte1 = WiFi.localIP()[0];
-    byte2 = WiFi.localIP()[1];
-    byte3 = WiFi.localIP()[2];
-    byte4 = WiFi.localIP()[3];
-    Serial.print("IP Address: ");
-    Serial.print(byte1);
-    Serial.print(".");
-    Serial.print(byte2);
-    Serial.print(".");
-    Serial.print(byte3);
-    Serial.print(".");
-    Serial.println(byte4);            
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Serial.print("Server state: "); Serial.println(isServerOn());    
+    //Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Firebase.setInt("/int",random(0, 30));
-  //delay(1000);
+  //messageHandle();
+
+  //Test messageHandle function
+  clientSetMessage("123456");
+  delay(1000);
+  clientSetMessage("1234567");
+//  if(isServerOn()){
+//    messageHandle();
+//  }
+  delay(500);
 }
